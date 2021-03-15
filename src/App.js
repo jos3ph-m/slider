f528ecd5893ff3bc98774624b24b766121e0af64
Joseph M <jmedina1985@hotmail.com>
2021-03-14 19:44:35 -0700

    Person section

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          // destructure data from each person
+          const { id, image, name, title, quote } = person;
+          return (
+            <article key={id}>
+              <img src={image} alt="name" className="person-img" />
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default App;
