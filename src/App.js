622b555e6e8c3f403ffa839b5309cfb6f50a4365
Joseph M <jmedina1985@hotmail.com>
2021-03-15 20:25:22 -0700

    prev and next buttons

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
         <button className="next">
